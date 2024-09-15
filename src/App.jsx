dd35186bd1a37c91e6d06b65f4d39c847687ca00
mikel161211231 <mikel.lopez@ikasle.aeg.eus>
2024-09-15 17:27:57 +0200

    [UPDATE] Import the necessaryu files on app.jsx and declare the referencies for the color elements.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react';
+import useSound from 'use-sound';
+import simon from "./assets/sounds/sprite.mp3";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  // Declarar las referencias a los elementos de colores
+  const blueRef = useRef(null);
+  const yellowRef = useRef(null);
+  const greenRef = useRef(null);
+  const reRef = useRef(null);
 
   return (
     ""
